b1b762e7548d2b2a55b84ce2b99fdf1c0997f1a1
juniorjwj <junio2063@gmail.com>
2022-10-20 16:47:23 -0300

    erro no yup

--- src/model/User.js
+++ src/model/User.js
@@ -17,89 +17,90 @@ module.exports = {
             avatar: user.avatar
         }))
     },
-    async create(newUser){
-        const db = await Database()
-        const data = await db.all(`SELECT email FROM user WHERE email = "${newUser.email}"`)
+    // async create(newUser){
+    //     const db = await Database()
+    //     const data = await db.all(`SELECT email FROM user WHERE email = "${newUser.email}"`)
         
-        if(data.length == 0){
-            try {
-                const db = await Database()
+    //     if(data.length == 0){
+    //         try {
+    //             const db = await Database()
     
-                await db.run(`INSERT INTO user (
-                    name,
-                    email,
-                    password, 
-                    avatar
-                ) VALUES (
-                    "${newUser.name}",
-                    "${newUser.email}",
-                    "${newUser.password}",
-                    "${newUser.avatar}"
-                )`)
-                await db.close()
-            } catch (error) {
-                console.log(error);
-            }
-        }else{
-            console.log("já existe um usuário com esse email")
-            console.log(error);
-        }
+    //             await db.run(`INSERT INTO user (
+    //                 name,
+    //                 email,
+    //                 password, 
+    //                 avatar
+    //             ) VALUES (
+    //                 "${newUser.name}",
+    //                 "${newUser.email}",
+    //                 "${newUser.password}",
+    //                 "${newUser.avatar}"
+    //             )`)
+    //             await db.close()
+    //         } catch (error) {
+    //             console.log(error);
+    //         }
+    //     }else{
+    //         console.log("já existe um usuário com esse email")
+    //         console.log(error);
+    //     }
 
         
-    },
-    // async create(newUser){
-    //     console.log(newUser)
-    //     // const db = await Database()
-    //     // const data = await db.all(`SELECT email FROM user WHERE email = "${newUser.email}"`)
-    //     const schema = yup.object().shape({
-    //         password: yup.string("Erro: Necessário preencher o campo senha!")
-    //           .required("Erro: Necessário preencher o campo senha!")
-    //           .min(8, "Erro: A senha deve ter no mínimo 6 caracteres!"),
-    //         email: yup.string("Erro: Necessário preencher o campo e-mail!")
-    //           .required("Erro: Necessário preencher o campo e-mail!")
-    //           .email("Erro: Necessário preencher o campo com e-mail válido!"),
-    //         name: yup.string("Erro: Necessário preencher o campo nome!")
-    //           .required("Erro: Necessário preencher o campo nome!")
-    //       });
-    //     try{
-    //         console.log("oi")
-    //         let falha = await schema.validate(newUser);
-    //         console.log(falha)
-    //     }catch(err){
-    //         return res.status(400).json({
-    //             erro: true,
-    //             message: error.message,
-    //             errors: error.inner
-    //           })
-              
-    //     }
-    //     return res.json({
-    //         erro: false,
-    //         mensagem: "Dados corretos!"
-    //       });
-    //     // if(data.length == 0){
-    //     //     try {
-    //     //         const db = await Database()
+    // },
+    async create(newUser){
+        console.log(newUser)
+        // const db = await Database()
+        // const data = await db.all(`SELECT email FROM user WHERE email = "${newUser.email}"`)
+        const schema = yup.object().shape({
+            password: yup.string("Erro: Necessário preencher o campo senha!")
+              .required("Erro: Necessário preencher o campo senha!")
+              .min(8, "Erro: A senha deve ter no mínimo 6 caracteres!"),
+            email: yup.string("Erro: Necessário preencher o campo e-mail!")
+              .required("Erro: Necessário preencher o campo e-mail!")
+              .email("Erro: Necessário preencher o campo com e-mail válido!"),
+            name: yup.string("Erro: Necessário preencher o campo nome!")
+              .required("Erro: Necessário preencher o campo nome!")
+          });
+        try{
+            console.log("oi")
+            await schema.validate(newUser)
+        }catch(err){
+            console.log(err)
+            return res.status(400).json({
+                erro: true,
+                message: err.message,
+                errors: err.errors
+              })
+        }
+        return res.json({
+            erro: false,
+            mensagem: "Dados corretos!"
+          });
+          
+        // if(data.length == 0){
+        //     try {
+        //         const db = await Database()
     
-    //     //         await db.run(`INSERT INTO user (
-    //     //             name,
-    //     //             email,
-    //     //             password, 
-    //     //             avatar
-    //     //         ) VALUES (
-    //     //             "${newUser.name}",
-    //     //             "${newUser.email}",
-    //     //             "${newUser.password}",
-    //     //             "${newUser.avatar}"
-    //     //         )`)
-    //     //         await db.close()
-    //     //     } catch (error) {
-    //     //         console.log(error);
-    //     //     }
-    //     // }else{
-    //     //     console.log("já existe um usuário com esse email")
-    //     //     console.log(error);
-    //     // }
+        //         await db.run(`INSERT INTO user (
+        //             name,
+        //             email,
+        //             password, 
+        //             avatar
+        //         ) VALUES (
+        //             "${newUser.name}",
+        //             "${newUser.email}",
+        //             "${newUser.password}",
+        //             "${newUser.avatar}"
+        //         )`)
+        //         await db.close()
+        //     } catch (error) {
+        //         console.log(error);
+        //     }
+        // }else{
+        //     console.log("já existe um usuário com esse email")
+        //     console.log(error);
+        // }
+    },
     async delete(id){
         const db = await Database()
 
