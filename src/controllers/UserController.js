bcd712bded40eeb4634bbc62aa0509c9b12c15bf
juniorjwj <junio2063@gmail.com>
2022-10-27 14:17:18 -0300

    Corrigindo nomes no import

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const User = require('../model/user')
+const User = require('../model/User')
 const { eAdmin } = require('../../middlewares/auth');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
--- src/route.js
+++ src/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const userController = require('./controllers/userController');
+const userController = require('./controllers/UserController');
 
 const { eAdmin } = require('../middlewares/auth');
 const multer = require("multer");
